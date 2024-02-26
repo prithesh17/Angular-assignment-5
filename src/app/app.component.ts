68053c4029b7f00daa834ed8035198aef3fb8c6e
Prithesh Salian <pritheshmarodi@gmail.com>
2024-02-26 21:55:47 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { UserFormComponent } from './user-form/user-form.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,UserFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
